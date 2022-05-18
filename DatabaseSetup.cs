778891f2f72a3c3b86144a42ce880592edd1f8a5
MayaTavaresIFSP <tavares.pereira@aluno.ifsp.edu.br>
2022-05-18 08:13:24 -0300

    commit

---
+++ DatabaseSetup.cs
@@ -0,0 +1,33 @@
+using Microsoft.Data.Sqlite;
+
+namespace LabManager.Database;
+
+class DatabaseSetup
+{
+    private DatabaseConfig databaseConfig;
+
+    public DatabaseSetup (DatabaseConfig databaseConfig)
+    {
+        this.databaseConfig = databaseConfig;
+        CreateComputerTable();
+    }
+
+    public void CreateComputerTable()
+    {
+        
+        var conection = new SqliteConnection("Data Source=database.db");
+        conection.Open();
+
+        var command = conection.CreateCommand();
+        command.CommandText = @";
+        CREATE TABLE IF NOT EXISTS Computers(
+            id int not null primary key,
+            ram varchar(100) not null,
+            processor varchar(100) not null
+        );
+        ";
+        command.ExecuteNonQuery();
+
+        conection.Close();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using LabManager.Database;
+using LabManager.Repositories;
+using Microsoft.Data.Sqlite;
+
+Console.WriteLine(args);
+
+foreach (var arg in args)
+{
+    Console.WriteLine(arg);
+}
+    var databaseConfig = new DatabaseConfig();
+    new DatabaseSetup(databaseConfig);
+
+    var computerRepository = new ComputerRepository(databaseConfig);
+
+    //Routing -- Roteamento
+
+
+    var modelName = args[0];
+    var modelAction = args[1];
+
+    if(modelName == "Computer")
+    {
+        if(modelAction == "List")
+        {
+           Console.WriteLine("Computer List");
+           foreach(var computer in computerRepository.GetAll())
+           {
+               Console.WriteLine("{0},{1},{2}", computer.Id, computer.Ram, computer.Processor);
+           }
+        }
+        if(modelAction == "New"){
+            var conection = new SqliteConnection("Data Source=database.db");
+            conection.Open();
+
+            Console.WriteLine("New computer");
+            int id = Convert.ToInt32(args[2]);
+            string ram = args[3];
+            string processor = args[4];
+
+            var command = conection.CreateCommand();
+            command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
+            command.Parameters.AddWithValue("$id", id);
+            command.Parameters.AddWithValue("$ram", ram);
+            command.Parameters.AddWithValue("$processor",processor);
+            command.ExecuteNonQuery();
+
+            conection.Close();
+
+            Console.WriteLine("{0}, {1}, {2}", id, ram, processor);
+        }
+    }
