6105922458b252abff172d6685c95b89475d2f52
MayaTavares <tavares.pereira@aluno.ifsp.edu.br>
2022-05-31 23:05:25 -0300

    ajustando GetAll()

--- LabManager/ComputerRepository.cs
+++ LabManager/ComputerRepository.cs
@@ -4,24 +4,31 @@ namespace LabManager.Repositories;
 
 class ComputerRepository
 {
+    private readonly DatabaseConfig databaseConfig;
+
     public List<Computer> GetAll()
     {
         var computers = new List<Computer>();
 
-    Console.WriteLine("Computer List");
+        var conection = new SqliteConnection(databaseConfig.ConnectionString);
+        connection.Open();
 
-           var reader = command.ExecuteReader();
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Computers";
+        var reader = command.ExecuteReader();
             
             while(reader.Read())
             {
                 var id = reader.GetInt32(0);
                 var ram = reader.GetString(1);
-                var processor = readerGetString(2);
-                var computer = new ComputerRepository(3);
+                var processor = reader.GetString(2);
+                var computer = new Computer(id, ram, processor);
 
+                computers.Add(computer);
             }
 
-           return computers;
+            conection.Close();
+            return computers;
         }
 
 }
