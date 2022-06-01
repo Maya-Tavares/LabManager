23750ac838b2f9f166bf8d4170bac0afa1b9bda2
MayaTavares <tavares.pereira@aluno.ifsp.edu.br>
2022-05-31 22:31:49 -0300

    ajustando modelAction New

--- Program.cs
+++ Program.cs
@@ -22,24 +22,26 @@ using LabManager.Repositories;
            }
         }
 
-        if(modelAction == "New"){
-            var conection = new SqliteConnection("Data Source=database.db");
-            conection.Open();
+        if(modelAction == "New")
+        {
 
-            Console.WriteLine("New computer");
-            int id = Convert.ToInt32(args[2]);
+            var id = Convert.ToInt32(args[2]);
             string ram = args[3];
             string processor = args[4];
 
-            var command = conection.CreateCommand();
-            command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
-            command.Parameters.AddWithValue("$id", id);
-            command.Parameters.AddWithValue("$ram", ram);
-            command.Parameters.AddWithValue("$processor",processor);
-            command.ExecuteNonQuery();
-
-            conection.Close();
-
-            Console.WriteLine("{0}, {1}, {2}", id, ram, processor);
+            var computer = new Computer(id, ram, processor);
+            computerRepository.Save(computer);
         }
+
+        //var command = conection.CreateCommand();
+        //command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
+        //command.Parameters.AddWithValue("$id", id);
+        //command.Parameters.AddWithValue("$ram", ram);
+        //command.Parameters.AddWithValue("$processor",processor);
+        //command.ExecuteNonQuery();
+
+        //conection.Close();
+
+        //Console.WriteLine("{0}, {1}, {2}", id, ram, processor);
+        
     }
