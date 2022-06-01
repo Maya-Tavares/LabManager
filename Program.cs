2ad6b5aa643f26e528523eb76c4e30382e9de153
MayaTavares <tavares.pereira@aluno.ifsp.edu.br>
2022-05-31 23:30:58 -0300

    add Lab New

--- Program.cs
+++ Program.cs
@@ -81,5 +81,29 @@ using LabManager.Repositories;
         
         connection.Close();
     }
+
+    if(modelAction == "New")
+    {
+        var id = Convert.ToInt32(args[2]);
+        var number = args[3];
+        string name = args[4];
+        string block = args[5];
+
+        var connection = new SqliteConnection("Data Source=database.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+
+        command.CommandText = "INSERT INTO Lab VALUES($id, $number, $name, $block)";
+        command.Parameters.AddWithValue("$id", id);
+        command.Parameters.AddWithValue("$number", number);
+        command.Parameters.AddWithValue("$name", name);
+        command.Parameters.AddWithValue("$block", block);
+
+        command.ExecuteNonQuery();
+
+        connection.Close();
+    }
+}
         
     }
